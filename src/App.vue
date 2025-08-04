3a06e7e60249ff19e680d3b0eb75c8c212c52314
Akimio521 <akimio2333@gmail.com>
2025-08-04 10:26:18 +0800

    使用 vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <v-layout class="rounded rounded-md border">
+    <v-navigation-drawer>
+      <h1 class="app-title">MediaTools</h1>
+      <v-list nav>
+        <v-list-item title="MediaTools" link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- title="MediaTools" -->
+    <v-app-bar></v-app-bar>
+
+    <v-main class="d-flex align-center justify-center" height="300">
+      <v-container>
+        <v-sheet
+          border="dashed md"
+          color="surface-light"
+          height="200"
+          rounded="lg"
+          width="100%"
+        ></v-sheet>
+      </v-container>
+    </v-main>
+  </v-layout>
 </template>
 
-<style scoped></style>
+<script lang="ts" setup>
+//
+</script>
+
+<style scoped>
+.app-title {
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin: 10px 0;
+  /* padding: 16px; */
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "system",
+  },
+});
+
+import App from "@/App.vue";
+import router from "@/router";
 
 const app = createApp(App);
-
+app.use(vuetify);
 app.use(router);
-
 app.mount("#app");
