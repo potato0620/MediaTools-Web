91ecb773a7ed2780186e69a5f64153d1e9d59109
Akimio521 <akimio2333@gmail.com>
2025-08-04 17:28:01 +0800

    feat: 添加图标集依赖

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -9,6 +10,9 @@ const vuetify = createVuetify({
   directives,
   theme: {
     defaultTheme: "system",
+  },
+  icons: {
+    defaultSet: "mdi",
   },
 });
 
