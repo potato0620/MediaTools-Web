7fcf4a3c7e3c306d8be543ea45daf2a720c62f82
Akimio521 <akimio2333@gmail.com>
2025-08-05 19:31:40 +0800

    feat(TMDBService): 添加TMDB服务，支持获取海报图像

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export * from "@/services/storage";
 export * from "@/services/media";
+export * from "@/services/tmdb";
---
+++ src/services/tmdb.ts
@@ -0,0 +1,16 @@
+import api from "@/services/api";
+import { type MediaItem } from "@/types";
+
+export const TMDBService = {
+  ImageService: {
+    /**
+     * 获取海报图像
+     * @param media_type 媒体类型
+     * @param tmdb_id TMDB ID
+     * @returns 海报图像的URL
+     */
+    async GetPosterImage(media_type: string, tmdb_id: number): Promise<string> {
+      return await api.get(`/tmdb/image/poster/${media_type}/${tmdb_id}`);
+    },
+  },
+};
