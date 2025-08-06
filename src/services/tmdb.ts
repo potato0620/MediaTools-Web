49610bfca310b904bb17680f7922e2615c91dc5d
Akimio521 <akimio2333@gmail.com>
2025-08-06 11:58:58 +0800

    feat(TMDBService): 添加获取媒体详情的方法

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -13,4 +13,14 @@ export const TMDBService = {
       return await api.get(`/tmdb/image/poster/${media_type}/${tmdb_id}`);
     },
   },
+
+  /**
+   * 获取媒体详情
+   * @param media_type 媒体类型
+   * @param tmdb_id TMDB ID
+   * @returns 媒体详情
+   */
+  async GetOverview(media_type: string, tmdb_id: number): Promise<string> {
+    return await api.get(`/tmdb/overview/${media_type}/${tmdb_id}`);
+  },
 };
